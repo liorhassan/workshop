89a51ed4ea451ba2fd420a4a596b01d2f81a45b5
shenhav <shenhavc@post.bgu.ac.il>
2020-04-07 13:59:56 +0300

    Added code for use case 2.6 + Prepared acceptance test for 2.6

--- workshopCode/src/DomainLayer/Basket.java
+++ workshopCode/src/DomainLayer/Basket.java
@@ -1,4 +1,8 @@
 package DomainLayer;
 
 public class Basket {
+
+    public void addProduct(Product p){
+        //TODO: implement
+    }
 }
--- workshopCode/src/DomainLayer/User.java
+++ workshopCode/src/DomainLayer/User.java
@@ -1,4 +1,14 @@
 package DomainLayer;
 
 public class User {
+
+    private ShoppingCart shoppingCart;
+
+    public User(){
+        shoppingCart = new ShoppingCart();
+    }
+
+    public ShoppingCart getShoppingCart() {
+        return shoppingCart;
+    }
 }
