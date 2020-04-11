6022a1ae60e9c061f2c6e06e0e9bbe8df0e60507
shauli genish <geniss@post.bgu.ac.il>
2020-04-11 15:01:21 +0300

    Use case 4.7 implementation

--- workshopCode/src/DomainLayer/User.java
+++ workshopCode/src/DomainLayer/User.java
@@ -1,14 +1,23 @@
 package DomainLayer;
 
+import java.util.HashMap;
+
 public class User {
+
+    private HashMap<Store,StoreManaging> storeManagments;
 
     private ShoppingCart shoppingCart;
 
     public User(){
-        shoppingCart = new ShoppingCart();
+        shoppingCart = new ShoppingCart(this);
+        storeManagments = new HashMap<>();
     }
 
     public ShoppingCart getShoppingCart() {
         return shoppingCart;
+    }
+
+    public void removeStoreManagment(Store store) {
+        storeManagments.remove(store);
     }
 }
