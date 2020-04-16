6afc444e1fc3817f2bebc01367534bbdb1402e1c
NoyEzra <noyez@post.bgu.ac.il>
2020-04-16 11:34:54 +0300

    UC2.6 - edit permissions

--- workshopCode/src/ServiceLayer/EditPermissions.java
+++ workshopCode/src/ServiceLayer/EditPermissions.java
@@ -1,4 +1,18 @@
 package ServiceLayer;
 
+import DomainLayer.Permission;
+import DomainLayer.SystemHandler;
+
+import java.util.List;
+
 public class EditPermissions {
+
+    public String execute(String userName, List<Permission> permissions, String storeName ){
+        try{
+            SystemHandler.getInstance().editPermissions(userName, permissions, storeName);
+        }
+        catch (Exception e){
+            return e.getMessage();
+        }
+    }
 }
