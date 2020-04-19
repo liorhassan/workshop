5309233893c093a73cbc104a335f938b5fb641db
shauli genish <geniss@post.bgu.ac.il>
2020-04-19 13:19:27 +0300

    Logger base implementation

--- workshopCode/src/DomainLayer/SystemLogger.java
+++ workshopCode/src/DomainLayer/SystemLogger.java
@@ -1,5 +1,9 @@
 package DomainLayer;
 
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 public class SystemLogger {
 
     private static SystemLogger ourInstance = new SystemLogger();
@@ -8,10 +12,32 @@ public class SystemLogger {
         return ourInstance;
     }
 
+    private final String eventLogURL = "/logs/EventLogger.log";
+    private final String errorLogURL = "/logs/ErrorLogger.log";
+    private Logger eventLogger;
+    private Logger errorLogger;
+    private FileHandler eventLog;
+    private FileHandler errorLog;
+
     private SystemLogger() {
+        try {
+            eventLog = new FileHandler(eventLogURL);
+            errorLog = new FileHandler(errorLogURL);
+            eventLogger = Logger.getLogger("Event Logger");
+            errorLogger = Logger.getLogger("Error Logger");
+            SimpleFormatter formatter = new SimpleFormatter();
+            eventLog.setFormatter(formatter);
+            errorLog.setFormatter(formatter);
+            eventLogger.addHandler(eventLog);
+            errorLogger.addHandler(errorLog);
+        } catch (Exception e){}
     }
 
-    public void writeAction(String action){}
+    public void writeEvent(String event){
+        eventLogger.info(event);
+    }
 
-    public void writeError(String error){}
+    public void writeError(String error){
+        errorLogger.info(error);
+    }
 }
