0e4c559a3c442e8e69b18de5da3fe3ae40e07ddb
nofar carmel <nofarcar@bgu.ac.il>
2020-04-21 12:34:33 +0300

    add logger

--- workshopCode/src/ServiceLayer/ViewUserPurchaseHistory.java
+++ workshopCode/src/ServiceLayer/ViewUserPurchaseHistory.java
@@ -2,15 +2,18 @@ package ServiceLayer;
 
 import DomainLayer.Purchase;
 import DomainLayer.SystemHandler;
+import DomainLayer.SystemLogger;
 import DomainLayer.UserPurchaseHistory;
 
 
 public class ViewUserPurchaseHistory {
 
     public String execute() {
+        SystemLogger.getInstance().writeEvent("View User Purchase History command");
         try {
             return getMessage(SystemHandler.getInstance().getUserPurchaseHistory());
         } catch (RuntimeException e) {
+            SystemLogger.getInstance().writeError("Register error: " + e.getMessage());
             return e.getMessage();
         }
     }
