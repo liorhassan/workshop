b078f5d6c85e0ef0018eb073b2825f4fdecfefdc
NoyEzra <noyez@post.bgu.ac.il>
2020-04-22 18:02:09 +0300

    UC2.8 - add logger

--- workshopCode/src/ServiceLayer/PurchaseCart.java
+++ workshopCode/src/ServiceLayer/PurchaseCart.java
@@ -1,15 +1,18 @@
 package ServiceLayer;
 
 import DomainLayer.SystemHandler;
+import DomainLayer.SystemLogger;
 
 public class PurchaseCart {
 
     public String execute() {
         try {
+            SystemLogger.getInstance().writeEvent("Purchase Cart command");
             return SystemHandler.getInstance().purchaseCart();
         }
 
         catch (Exception e) {
+            SystemLogger.getInstance().writeError("Purchase Cart error: " + e.getMessage());
             return e.getMessage();
         }
     }
