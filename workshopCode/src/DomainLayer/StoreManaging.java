982e50bff7bd5b63e00d31859a341ee1efbbefb7
NoyEzra <noyez@post.bgu.ac.il>
2020-04-22 17:49:24 +0300

    UC4.6 - changed permissions field from user to storeManager and storeOwner

--- workshopCode/src/DomainLayer/StoreManaging.java
+++ workshopCode/src/DomainLayer/StoreManaging.java
@@ -1,14 +1,29 @@
 package DomainLayer;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class StoreManaging {
 
     User appointer;
+    private List<Permission> permission;
 
     public StoreManaging(User appointer) {
         this.appointer = appointer;
+        this.permission = new LinkedList<>();
+
     }
 
     public User getAppointer() {
         return appointer;
     }
+
+    public List<Permission> getPermission(){
+        return  permission;
+    }
+
+    public void setPermission(List<Permission> p){
+        permission = p;
+    }
+
 }
--- workshopCode/src/DomainLayer/StoreOwning.java
+++ workshopCode/src/DomainLayer/StoreOwning.java
@@ -1,18 +1,28 @@
 package DomainLayer;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class StoreOwning {
 
     User appointer;
+    private List<Permission> permission;
 
     // first owner constructor
     public StoreOwning() {}
 
     public StoreOwning(User appointer) {
         this.appointer = appointer;
+        this.permission = new LinkedList<>();
     }
 
     public User getAppointer() {
         return appointer;
     }
+
+    public List<Permission> getPermission(){
+        return  permission;
+    }
+
 
 }
