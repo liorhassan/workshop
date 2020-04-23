73a3298319f98d171c9929d3814bbd2949a8befa
nofar carmel <nofarcar@bgu.ac.il>
2020-04-23 21:02:11 +0300

    Completed case 3.7 function structure fix and 3.7 acceptance tests

--- workshopCode/tests/AcceptanceTests/UC3_7.java
+++ workshopCode/tests/AcceptanceTests/UC3_7.java
@@ -1,7 +1,8 @@
 package AcceptanceTests;
 
 import DomainLayer.SystemHandler;
-import ServiceLayer.ViewUserPurchaseHistory;
+import ServiceLayer.UsersHandler;
+import ServiceLayer.ViewPurchaseHistoryHandler;
 import org.junit.*;
 
 import java.util.HashMap;
@@ -10,27 +11,26 @@ import static org.junit.Assert.assertEquals;
 
 public class UC3_7 {
 
-    private ViewUserPurchaseHistory command;
+    private static ViewPurchaseHistoryHandler historyHandlerhandler;
+    private static UsersHandler usersHandler;
 
     @BeforeClass
-    public static void init() throws Exception{
-        SystemHandler.getInstance().register("nufi");
-        SystemHandler.getInstance().login("nufi");
-    }
-
-    @Before
-    public void setUp() throws Exception {
-        command = new ViewUserPurchaseHistory();
+    public static void init() throws Exception {
+        usersHandler = new UsersHandler();
+        usersHandler.register("nufi", "123456");
+        usersHandler.login("nufi", "123456");
+        historyHandlerhandler = new ViewPurchaseHistoryHandler();
     }
 
     @AfterClass
     public static void tearDown() throws Exception {
+        // TODO: fix!!!!!!!!!!!!!!
         SystemHandler.getInstance().setUsers(new HashMap<>());
     }
 
     @Test
     public void valid() {
-        String result = command.execute();
+        String result = historyHandlerhandler.ViewPurchaseHistoryOfUser();
         assertEquals("Shopping history:"+"\n", result);
     }
 }
