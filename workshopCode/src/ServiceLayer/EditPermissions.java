f7c8ff0e99a62d8c6d1ab29687e5247c41a6101d
NoyEzra <noyez@post.bgu.ac.il>
2020-04-23 12:13:26 +0300

    UC4.6 - fixed logger

--- workshopCode/src/ServiceLayer/EditPermissions.java
+++ workshopCode/src/ServiceLayer/EditPermissions.java
@@ -10,8 +10,23 @@ public class EditPermissions {
 
     public String execute(String userName, List<Permission> permissions, String storeName ){
         try{
-            SystemLogger.getInstance().writeEvent("Edit Permissions command");
-            return SystemHandler.getInstance().editPermissions(userName, permissions, storeName);
+
+            String result =  SystemHandler.getInstance().editPermissions(userName, permissions, storeName);
+
+            //logger
+            String permissionsStr;
+            if(permissions.isEmpty()){
+                permissionsStr = "empty permissions list";
+            }
+            else{
+                permissionsStr = "{";
+                for(Permission p: permissions){
+                    permissionsStr = permissionsStr.concat(p.getAllowedAction() + ", ");
+                }
+                permissionsStr = permissionsStr + "}";
+            }
+            SystemLogger.getInstance().writeEvent("Edit Permissions command: user name - " + userName + ", permissions - " + permissionsStr + ", store name - " + storeName);
+            return result;
         }
         catch (Exception e){
             SystemLogger.getInstance().writeError("Edit Permissions error: " + e.getMessage());
