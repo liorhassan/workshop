4fbf829c6e542e267b361ba587547891b50a1b28
nofar carmel <nofarcar@bgu.ac.il>
2020-04-23 21:41:17 +0300

    complete acceptance test fix for UC 3.2

--- workshopCode/tests/AcceptanceTests/UC3_7.java
+++ workshopCode/tests/AcceptanceTests/UC3_7.java
@@ -1,29 +1,28 @@
 package AcceptanceTests;
 
-import DomainLayer.SystemHandler;
 import ServiceLayer.UsersHandler;
 import ServiceLayer.ViewPurchaseHistoryHandler;
-import org.junit.*;
-
-import java.util.HashMap;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class UC3_7 {
 
-    private static ViewPurchaseHistoryHandler historyHandlerhandler;
-    private static UsersHandler usersHandler;
+    private ViewPurchaseHistoryHandler historyHandlerhandler;
+    private UsersHandler usersHandler;
 
     @BeforeClass
-    public static void init() throws Exception {
+    public void init() throws Exception {
         usersHandler = new UsersHandler();
+        historyHandlerhandler = new ViewPurchaseHistoryHandler();
         usersHandler.register("nufi", "123456");
         usersHandler.login("nufi", "123456");
-        historyHandlerhandler = new ViewPurchaseHistoryHandler();
     }
 
     @AfterClass
-    public static void tearDown() throws Exception {
+    public void tearDown() throws Exception {
         usersHandler.resetUsers();
     }
 
