2f9a252f606a7cea52b87697cfca88975731dd21
shauli genish <geniss@post.bgu.ac.il>
2020-06-04 21:16:20 +0300

    fixes for the web socket implementation for future conccurency implementation

--- workshopCode/src/PresentationLayer/js/MainWindow.js
+++ workshopCode/src/PresentationLayer/js/MainWindow.js
@@ -3,7 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
     var ws = new WebSocket("ws://localhost:8088");
     ws.onopen = function(){
         ws.onmessage = function(event){
-            alert(event.data);
+            var message = JSON.parse(event.data);
+            if(message.session_id)
+                localStorage.setItem("session_id",message.session_id);
+            else
+                message.forEach(element => {alert(element)});
         };
     }
     window.addEventListener("storage",function(e){
