d58a709d9879ce61501928eff7740410c6822f2a
nofar carmel <nofarcar@bgu.ac.il>
2020-06-24 23:42:34 +0300

    PurchaseTest UT works

--- workshopCode/tests/UnitTests/PurchaseTest.java
+++ workshopCode/tests/UnitTests/PurchaseTest.java
@@ -1,10 +1,8 @@
 package UnitTests;
 
 import DataAccessLayer.PersistenceController;
-import DomainLayer.TradingSystem.Models.Product;
-import DomainLayer.TradingSystem.Models.Purchase;
-import DomainLayer.TradingSystem.Models.ShoppingCart;
-import DomainLayer.TradingSystem.Models.Store;
+import DomainLayer.TradingSystem.Models.*;
+import DomainLayer.TradingSystem.StoreOwning;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -23,7 +21,9 @@ public class PurchaseTest {
 
     @Before
     public void setUp() throws Exception {
-        purchase = new Purchase(new ShoppingCart(null));
+        User user0 = new User();
+        user0.setUsername("tester0");
+        purchase = new Purchase(new ShoppingCart(user0));
     }
 
     @After
@@ -35,7 +35,13 @@ public class PurchaseTest {
         assertEquals(0,purchase.getTotalCheck(),0.0000000001);
         Product p1 = new Product("Shirt",null,null,40.0,"Fox",1);
         Product p2 = new Product("Dress",null,null,45.5,"Fox",1);
-        Store store = new Store("Fox",null,null,null);
+        PersistenceController.create(p1);
+        PersistenceController.create(p2);
+        User user = new User();
+        user.setUsername("tester");
+        StoreOwning storeOwning = new StoreOwning("Fox", "tester");
+        Store store = new Store("Fox",null,user,storeOwning);
+        PersistenceController.create(store);
         store.getInventory().put(p1,10);
         store.getInventory().put(p2,10);
         purchase.getPurchasedProducts().addProduct("Shirt",store,1, true);
